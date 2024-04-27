3cf2be5219fef715ce8aa0ba5bc49ccac511d894
harichandanmohanty <harichandanmohanty@gmail.com>
2024-04-27 12:43:02 +0530

    Todo app with provider and changeNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    const title = 'Todo Vanilla';
+    const title = 'My Todos';
     return ChangeNotifierProvider(
       create: (BuildContext context) => AppStateNotifier(),
       child: MaterialApp(
